f83aba2800bc81c573658ec6d0d5d9f7d04c76c3
Eric Pfahl <eric.pfahl@gmail.com>
2023-09-10 15:03:05 -0400

    update mix for hex package

--- mix.exs
+++ mix.exs
@@ -1,27 +1,53 @@
 defmodule PairingHeap.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :pairing_heap,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: @version,
+      elixir: "~> 1.15",
+      description: description(),
+      package: package(),
+      docs: docs(),
+      source_url: "https://github.com/epfahl/pairing_heap",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Elixir implementation of a pairing heap."
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Eric Pfahl"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/epfahl/pairing_heap"
+      }
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "PairingHeap",
+      extras: ["README.md"]
     ]
   end
 end
